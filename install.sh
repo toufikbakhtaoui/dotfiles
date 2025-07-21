60062809e4e69f249e5435cafa6a24be4e11f598
toufik Bakhtaoui <toufik.bakhtaoui@gmail.com>
2025-07-21 17:36:06 +0200

    Intall Homebrew

--- install.sh
+++ install.sh
@@ -24,9 +24,32 @@ if ! xcode-select -p &>/dev/null; then
     sudo xcodebuild -license accept
 fi
 
-
 # ─────────────────────────────────────────────
-# Start chezmoi
+# Install Homebrew 
 # ─────────────────────────────────────────────
 
+log info "✓ Checking Homebrew..."
+
+  if ! command -v brew &>/dev/null; then
+    log info "📦 Installing Homebrew..."
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || \
+      fail "Homebrew installation failed."
+
+    if [[ -x "/opt/homebrew/bin/brew" ]]; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+      echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
+    elif [[ -x "/usr/local/bin/brew" ]]; then
+      eval "$(/usr/local/bin/brew shellenv)"
+      echo 'eval "$(/usr/local/bin/brew shellenv)"' >> "$HOME/.zprofile"
+    else
+      fail "Homebrew was installed but not found in standard locations."
+    fi
+  else
+    log success "✅ Homebrew is already installed."
+    eval "$(brew shellenv)"
+  fi
+# ─────────────────────────────────────────────
+# Install and start chezmoi
+# ─────────────────────────────────────────────
+brew install chezmoi
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --verbose --apply toufikbakhtaoui

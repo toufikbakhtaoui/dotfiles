8152f23f469546f5d8894e0ff258e5fbba3ef196
toufik Bakhtaoui <toufik.bakhtaoui@gmail.com>
2025-07-21 17:17:47 +0200

    setup command line tools

--- install.sh
+++ install.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
 # ─────────────────────────────────────────────
-#  Start macos configuration setup
+# Start macos configuration setup
 # ─────────────────────────────────────────────
 
 set -eo pipefail
+
+LOG_PATH="${CHEZMOI_SOURCE_DIR:-$(chezmoi source-path)}/scripts/log.sh"
+source "$LOG_PATH"
+
+# ─────────────────────────────────────────────
+# Install Xcode ClI Tools
+# ─────────────────────────────────────────────
+
+log info "✓ Checking Xcode CLI Tools"
+if ! xcode-select -p &>/dev/null; then
+    log info "→ Installing Xcode CLI Tools"
+    xcode-select --install || fail "Failed to install Xcode CLI Tools"
+    # Wait for installation to complete
+    while ! xcode-select -p &>/dev/null; do
+        sleep 5
+    done
+    sudo xcodebuild -license accept
+fi
+
+
+# ─────────────────────────────────────────────
+# Start chezmoi
+# ─────────────────────────────────────────────
 
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --verbose --apply toufikbakhtaoui

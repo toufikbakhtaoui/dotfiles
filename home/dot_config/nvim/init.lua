994463de58dcf5e847eb7720af6bc010e1059bc5
toufik Bakhtaoui <toufik.bakhtaoui@gmail.com>
2025-07-27 22:41:37 +0200

    delete all plugins and add neotree simple configuration

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -14,12 +14,5 @@ local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
 require('lazy').setup({
-    require 'plugins.colorscheme',
-    require 'plugins.neotree',
-    require 'plugins.bufferline',
-    require 'plugins.lualine',
-    require 'plugins.treesitter',
-    require 'plugins.telescope',
-    require 'plugins.lsp',
-    require 'plugins.mason'
+     require 'plugins.neotree'        
 })
--- home/dot_config/nvim/lua/plugins/neotree.lua
+++ home/dot_config/nvim/lua/plugins/neotree.lua
@@ -1,63 +1,19 @@
 return {
-  -- Main Neo-tree plugin
-  'nvim-neo-tree/neo-tree.nvim',
-
-  branch = 'v3.x', -- Use the latest v3.x branch
-
-  dependencies = {
-    'nvim-lua/plenary.nvim',          -- Utility functions
-    'nvim-tree/nvim-web-devicons',    -- File and folder icons
-    'MunifTanjim/nui.nvim',           -- UI components
-  },
-
-  config = function()
-    -- Define diagnostic icons (optional)
-    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
-    vim.fn.sign_define('DiagnosticSignWarn',  { text = ' ', texthl = 'DiagnosticSignWarn' })
-    vim.fn.sign_define('DiagnosticSignInfo',  { text = ' ', texthl = 'DiagnosticSignInfo' })
-    vim.fn.sign_define('DiagnosticSignHint',  { text = '󰌵', texthl = 'DiagnosticSignHint' })
-
-    require('neo-tree').setup {
-      -- General options
-      close_if_last_window = true, -- Close Neo-tree if it's the last window
-      enable_git_status = true,    -- Show git changes
-      enable_diagnostics = true,   -- Show LSP diagnostics
-
-      -- Window settings
-      window = {
-        position = 'left',
-        width = 30,
-        mappings = {
-          ['<cr>'] = 'open',        -- Enter to open file
-          ['l'] = 'open',           -- l to open file
-          ['h'] = 'close_node',     -- h to collapse folder
-          ['a'] = 'add',            -- Create new file
-          ['d'] = 'delete',         -- Delete file
-          ['r'] = 'rename',         -- Rename file
-          ['q'] = 'close_window',   -- Close Neo-tree
-          ['R'] = 'refresh',        -- Refresh tree
-        },
-      },
-
-      -- Filesystem settings
-      filesystem = {
-        filtered_items = {
-          visible = false, -- Do not show hidden files by default
-          hide_dotfiles = true,
-          hide_gitignored = true,
-        },
-        follow_current_file = { enabled = true }, -- Auto-focus the current file
-        hijack_netrw_behavior = 'open_default',   -- Replace netrw
-      },
-
-      -- Buffers source (manage open files)
-      buffers = {
-        follow_current_file = { enabled = true },
-      },
-    }
-
-    -- Keybinding to toggle Neo-tree globally
-    vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { noremap = true, silent = true })
-  end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim"
+    },
+    config = function()
+        require("neo-tree").setup {
+          close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab  
+          popup_border_style = 'rounded',
+          enable_git_status = true,
+        }
+        vim.keymap.set('n', '<leader>e', ':Neotree toggle position=left<CR>', { noremap = true, silent = true }) -- focus file explorer
+        vim.keymap.set('n', '<leader>ngs', ':Neotree float git_status<CR>', { noremap = true, silent = true }) -- open git status window
+    end
 }
 

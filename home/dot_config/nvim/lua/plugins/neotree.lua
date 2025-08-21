df070d8d8ed8f7c743b65c580e379b46207c34ce
toufik Bakhtaoui <toufik.bakhtaoui@gmail.com>
2025-08-22 00:33:43 +0200

    add conform plugin to nvim for formatting files

--- home/dot_config/nvim/lua/plugins/neotree.lua
+++ home/dot_config/nvim/lua/plugins/neotree.lua
@@ -1,19 +1,18 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim"
-    },
-    config = function()
-        require("neo-tree").setup {
-          close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab  
-          popup_border_style = 'rounded',
-          enable_git_status = true,
-        }
-        vim.keymap.set('n', '<leader>e', ':Neotree toggle position=left<CR>', { noremap = true, silent = true }) -- focus file explorer
-        vim.keymap.set('n', '<leader>ngs', ':Neotree float git_status<CR>', { noremap = true, silent = true }) -- open git status window
-    end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+			popup_border_style = "rounded",
+			enable_git_status = true,
+		})
+		vim.keymap.set("n", "<leader>e", ":Neotree toggle position=left<CR>", { noremap = true, silent = true }) -- focus file explorer
+		vim.keymap.set("n", "<leader>ngs", ":Neotree float git_status<CR>", { noremap = true, silent = true }) -- open git status window
+	end,
 }
-
--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "java", "lua", "json", "yaml", "html", "css", "vue", "dockerfile", "markdown" },
-            highlight = { enable = true },
-            autotag = { enable = true }
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "java", "lua", "json", "yaml", "html", "css", "vue", "dockerfile", "markdown" },
+			highlight = { enable = true },
+			autotag = { enable = true },
+		})
+	end,
 }
